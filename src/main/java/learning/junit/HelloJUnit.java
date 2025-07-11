c0a73a33d146a84207be37e3679f630ecc1b5a0e
BhekumusaEric <bhekumusaeric@gmail.com>
2025-07-11 09:44:09 +0200

    Java TTD

---
+++ src/main/java/learning/junit/HelloJUnit.java
@@ -0,0 +1,12 @@
+package learning.junit;
+
+public class HelloJUnit {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public String greeting(String name) {
+        return "Welcome , " + name;
+    }
+}
